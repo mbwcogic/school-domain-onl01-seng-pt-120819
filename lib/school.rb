e208d43f16a41a95de7204ce85a6a1a9f5278ea6
Michael Waldo <mbwcogic@yahoo.com>
2020-01-13 02:34:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,8 @@
 class School 
-  attr_accessor :name, :roster 
+  attr_accessor :roster 
 
   def initialize(name)
-    @name = name 
+   
     @roster = {}
   end
   def add_student(student, grade)
